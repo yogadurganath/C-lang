4271034701b85416a27622f3c8782619c586f182
yogadurganath <38928275+yogadurganath@users.noreply.github.com>
2018-07-17 16:22:29 +0530

    Update odd bt 2 ints.c

--- odd bt 2 ints.c
+++ odd bt 2 ints.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    int n1,n2,i;
+    int n1,n2,i,c=0;
     scanf("%d%d",&n1,&n2);
     for(i=n1+1;i<n2;i++)
     {
+        if(c==0)
+        {
         if(i%2==1){
-            printf("%d ",i);
+            printf("%d",i);
+            c++;
+        }
+        }
+        else
+        {
+                   if(i%2==1)
+            printf(" %d",i);
         }
     }
 return 0;
