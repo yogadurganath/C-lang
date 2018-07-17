5256c0886bf2a4b0f9ef80dfb86d1b9d9d0fbf6e
yogadurganath <38928275+yogadurganath@users.noreply.github.com>
2018-07-17 11:27:07 +0530

    power

---
+++ pow.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int a,b,ans;
+    printf("Enter 2 nos");
+    scanf("%d%d",&a,&b);
+    ans=pow(a,b);
+   printf("%d",ans);
+   return(0);
+}
