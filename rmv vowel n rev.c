369ebbde7fe44bb0fa0ba9c4c6ddc1de6f1eaed0
yogadurganath <38928275+yogadurganath@users.noreply.github.com>
2018-07-21 09:52:37 +0530

    remove vowels and reverse string

---
+++ rmv vowel n rev.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+int vowelcheck(char);
+int main()
+{
+char n,s[100], t[100],a;
+int i, j = 0;
+scanf("%d",&n);
+scanf("%s",s);
+a=strlen(s);
+if(n==a)
+{
+for(i = 0; s[i] != '\0'; i++) {
+if(vowelcheck(s[i]) == 0){
+t[j] = s[i];
+j++;
+}
+}
+t[j] = '\0';
+strcpy(s, t);
+strrev(s);
+printf("%s", s);
+}
+return 0;
+}
+int vowelcheck(char c)
+{
+  switch(c) {
+    case 'a':
+    case 'A':
+    case 'e':
+    case 'E':
+    case 'i':
+    case 'I':
+    case 'o':
+    case 'O':
+    case 'u':
+    case 'U':
+      return 1;
+    default:
+      return 0;
+  }
+}
